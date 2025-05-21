cb276da636486e8e3042e68560acbb91e6dec082
Lucas Hein <lucasrafaelhein365@gmail.com>
2025-05-21 16:21:10 -0300

    Adionado a opção de cadastrar administrador e adicionado notice para o controller.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -10,6 +10,19 @@ class AdminsBackoffice::AdminsController < ApplicationController
   def edit
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso"
+    else
+      render :new
+    end
+  end
+
   def update
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso"
