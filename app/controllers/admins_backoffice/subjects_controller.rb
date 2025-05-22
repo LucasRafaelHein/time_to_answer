76bf6f38eaaf4c5a7bc6ed370924c8bbe764d8e4
Lucas Hein <lucasrafaelhein365@gmail.com>
2025-05-22 16:39:10 -0300

    Criando as views (new e edit) para os Assuntos/Áreas e ajustado os models I18n.

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -2,7 +2,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   before_action :set_subject, only: [:edit, :update, :destroy]
 
   def index
-    @subjects = Subject.all
+    @subjects = Subject.all.order(:description)
   end
 
   def edit
